09cca849f0ceb745177c185648dd0f4a3cb61efb
mjhmjh1104 <mjhmjh1104@naver.com>
2017-08-08 17:41:53 +0900

    updated

--- app.js
+++ app.js
@@ -100,6 +100,7 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function() {
+var post = process.env.PORT || 3000;
+http.listen(port, function() {
   console.log('> Server on!');
 });
